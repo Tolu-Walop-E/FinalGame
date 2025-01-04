1f799e4ed3d226b50d2b4a9c1e04a79c38ad4203
Mithusen <ec22960@qmul.ac.uk>
2025-01-04 21:21:25 +0000

    added pause menu to lvl1 and added more  to main menu

--- Assets/Scripts/MenuScripts/LevelSelector.cs
+++ Assets/Scripts/MenuScripts/LevelSelector.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
     public GameObject LevelSelectMenu;
     public GameObject[] levels;
-   
+    public string[] levelNames = {"Level-1-Forest","Level-2-Sandstorm","Level-3-Oasis","","Level-5-Ice-Level"};
+    public bool[] levelUnlocked = {true,false,false,false,false};
+    private Button playButton;
+
     void Start()
     {
         foreach (GameObject level in levels)
@@ -39,6 +44,11 @@ public class LevelSelector : MonoBehaviour
         }
     }
 
+    public void PlayLevel(){
+        int currentIndex = GetActivePanelIndex();
+        SceneManager.LoadScene(levelNames[currentIndex]);
+    }
+
     public void NextPanelRight()
     {
         int currentIndex = GetActivePanelIndex();
@@ -48,6 +58,14 @@ public class LevelSelector : MonoBehaviour
             levels[currentIndex].SetActive(false);
             int nextIndex = (currentIndex + 1) % levels.Length;
             levels[nextIndex].SetActive(true);
+            GameObject buttonObject = GameObject.Find("PlayButton");
+            playButton = buttonObject.GetComponent<Button>();
+            if(!(levelUnlocked[nextIndex]))
+            {
+                playButton.interactable = false;
+            }else{
+                playButton.interactable = true;
+            }
         }
     }
 
@@ -59,12 +77,28 @@ public class LevelSelector : MonoBehaviour
             levels[currentIndex].SetActive(false);
             int nextIndex = levels.Length - 1;
             levels[nextIndex].SetActive(true);
+            GameObject buttonObject = GameObject.Find("PlayButton");
+            playButton = buttonObject.GetComponent<Button>();
+            if(!(levelUnlocked[nextIndex]))
+            {
+                playButton.interactable = false;
+            }else{
+                playButton.interactable = true;
+            }
         }
         else if (currentIndex != -1)
         {
             levels[currentIndex].SetActive(false);
             int nextIndex = (currentIndex - 1) % levels.Length;
             levels[nextIndex].SetActive(true);
+            GameObject buttonObject = GameObject.Find("PlayButton");
+            playButton = buttonObject.GetComponent<Button>();
+            if(!(levelUnlocked[nextIndex]))
+            {
+                playButton.interactable = false;
+            }else{
+                playButton.interactable = true;
+            }
         }
     }
 
@@ -80,5 +114,13 @@ public class LevelSelector : MonoBehaviour
         }
 
         return -1; 
+    }
+
+    public void UnlockAll()
+    {   
+        for (int i = 0; i < 5; i++)
+        {
+            levelUnlocked[i] = true;
+        }
     }
 }
